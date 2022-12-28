a3ebcd769ceb06c7dfb8fdf7d1683a9b87159bf2
Adam Coddington <adam.coddington@airship.com>
2022-12-27 19:38:16 -0800

    Bump version: 1.1.0 → 1.2.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name="logitech-flow-kvm",
-    version="1.1.0",
+    version="1.2.0",
     license="MIT",
     description=(
         "Quickly switch between paired devices when using a mouse"
